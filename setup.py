7e3a95e3e873bd0ec10009367537e8ff9e390095
Mike Winstead <mwinstead3790@gmail.com>
2017-05-22 19:51:53 -0500

    Added CLI script

--- setup.py
+++ setup.py
@@ -5,5 +5,6 @@ from distutils.core import setup
 setup(
     name="pybytecompile",
     version="1.0.0",
-    packages=["pybytecompile"]
+    packages=["pybytecompile"],
+    scripts=["pybytecompile/bin/pybytecompile.py"]
 )
