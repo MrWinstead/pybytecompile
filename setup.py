73398066749c14dee502d56dc591ac138bfb7c2c
Mike Winstead <mwinstead3790@gmail.com>
2017-05-22 21:04:10 -0500

    implemented upload to pypi

--- setup.py
+++ setup.py
@@ -3,8 +3,12 @@
 from distutils.core import setup
 
 setup(
+    description="Wrapper around compileall",
+    url="https://github.com/MrWinstead/pybytecompile.git",
     name="pybytecompile",
-    version="1.0.0",
+    version="1.0.1",
     packages=["pybytecompile"],
-    scripts=["bin/pybytecompiler.py"]
+    scripts=["bin/pybytecompiler.py"],
+    author="Mike Winstead",
+    author_email="mike@winstead.us"
 )
